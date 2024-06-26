3b352ba6ebeab5d3351abb38847a1e2b10445db1
VEGETABLE MAN WHERE ARE YOU? <rupee>
2024-06-26 13:16:28 -0700

    rought loop with shutdown handler

--- EDJShellAH.py
+++ EDJShellAH.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
 
-import os, time, glob, json
+import sys, os, time, glob, json
 
 ProgramName="ED Journalist"
 FakeInput=False
@@ -13,23 +13,28 @@ else:
     CurJour=max(glob.glob("/shared/Journal*"), key=os.path.getctime)
 
 print(ProgramName)
-
-import time
 time.sleep(1.5)
+print()
 
-global JournalFile
+IgnoredEvents=['Music']
+InterestingEvents=['Fileheader',
+        'Cargo',
+        'ReceiveText',
+        'ReservoirReplenished',
+        'Shutdown',
+        ]
+
+def handle_Shutdown(event):
+    print("Game shut down")
+    sys.exit(0)
+
+########################################################################################################
+
 JournalFile=open(CurJour, 'r')
 Quit=False
 
 N=0
 Events=[]
-IgnoredEvents=['Music']
-
-def Absorb(event):
-    eventType=event['event']
-    if not eventType in IgnoredEvents:
-        print(Events.index(event), eventType, event)
-
 while not Quit:
     jsonLine=JournalFile.readline() #why it don't block
     time.sleep(0.005)
@@ -47,12 +52,27 @@ while not Quit:
         time.sleep(2)
         raise Exception("oh shit")
 
-    #print(event['event'])
     Events.append(event)
-    Absorb(event)
+    eventType=event['event']
+    if eventType in InterestingEvents:
+        print('<'+eventType+'>', event)
+        if len(str(event)) > 80:
+            print()
+    elif not eventType in IgnoredEvents:
+        print("|%7d.) " % Events.index(event), end='')
+        print(".. %8d bytes (%s)" % (len(str(event)), eventType))
+    else:
+        pass
+
+    handlerName = "handle_"+eventType
+    if handlerName in dir():
+        print("Calling ", handlerName)
+        exec("%s(event)" % (handlerName))
+    else:
+        print("Not found %s in %s" % ( handlerName, dir()))
 
 
-
+# :      0.) <Fileheader> {'timestamp': '2024-06-24T04:29:02Z', 'event': 'Fileheader', 'part': 1, 'language': 'English/UK', 'Odyssey': True, 'gameversion': '4.0.0.1806', 'build': 'r302447/r0 '}
 
 
 
