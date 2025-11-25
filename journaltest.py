04269d75dc273e90970d2b34dc951282a2523a47
topromulan <lime.dale@gmail.com>
2025-11-24 22:40:29 -0800

    journal test exit cleanup

--- journaltest.py
+++ journaltest.py
@@ -7,24 +7,29 @@ from time import time, sleep
 
 lasttime=time()
 
-while True:
-    event=journal.read()
-
-    if not event:
-        sleep(0.5)
-        continue
-    print("%s [%s]" % (event['timestamp'], event['event']), end=" ")
-
-    eventcopy=event.copy()
-    del eventcopy['event']
-    del eventcopy['timestamp']
-    pp(eventcopy)
+try:
+    while True:
+        event=journal.read()
     
-    timenow=time()
-    sleep(1 if (1 > timenow-lasttime > 0.1) else 0)
-    lasttime=timenow
-
-    if("Shutdown" in event['event']):
-        break
-
-    print()
+        if not event:
+            sleep(0.5)
+            continue
+        print("%s [%s]" % (event['timestamp'], event['event']), end=" ")
+    
+        eventcopy=event.copy()
+        del eventcopy['event']
+        del eventcopy['timestamp']
+        pp(eventcopy)
+        
+        timenow=time()
+        sleep(1 if (1 > timenow-lasttime > 0.1) else 0)
+        lasttime=timenow
+    
+        if("Shutdown" in event['event']):
+            break
+    
+        print()
+except:
+    pass
+finally:
+    print("Journal Test Exiting")
